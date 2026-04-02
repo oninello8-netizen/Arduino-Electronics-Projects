91af3f867757bf6a69f5d935cfa64693bd080373
oninello8-netizen <oninello8@gmail.com>
2026-04-02 20:50:01 +0800

    Update 1.)16 Running Led.ino

--- 1.)16 Running Led.ino
+++ 1.)16 Running Led.ino
@@ -1,41 +1,20 @@
-void setup()
-{
-  for (int i = 0; i <= 13; i++)
-  {
+void setup() {
+  // 15 covers pins 0-13, plus 14 (A0) and 15 (A1)
+  for (int i = 0; i <= 15; i++) {
     pinMode(i, OUTPUT);
   }
-  pinMode(A0, OUTPUT);
-  pinMode(A1, OUTPUT);
 }
 
-void loop()
-{
-
-  for (int i = 0; i <= 13; i++)
-  {
+void loop() {
+  // Forward Chase: 0 to A1
+  for (int i = 0; i <= 15; i++) {
     digitalWrite(i, HIGH);
     delay(50);
     digitalWrite(i, LOW);
   }
 
-  digitalWrite(A0, HIGH);
-  delay(50);
-  digitalWrite(A0, LOW);
-
-  digitalWrite(A1, HIGH);
-  delay(50);
-  digitalWrite(A1, LOW);
-
-  digitalWrite(A1, HIGH);
-  delay(50);
-  digitalWrite(A1, LOW);
-
-  digitalWrite(A0, HIGH);
-  delay(50);
-  digitalWrite(A0, LOW);
-
-  for (int i = 13; i >= 0; i--)
-  {
+  // Backward Chase: A1 down to 0
+  for (int i = 15; i >= 0; i--) {
     digitalWrite(i, HIGH);
     delay(50);
     digitalWrite(i, LOW);
